308232f9fa6689ed94d9fd1c617a71ca711e0276
Thomas Choquet <tchoquet74@gmail.com>
2018-01-18 14:54:05 +0100

    ajout du fichier JS

---
+++ script.js
@@ -0,0 +1,6 @@
+for (var i = 0; i < document.getElementsByClassName('picto').length; i++) {
+  document.getElementsByClassName('picto')[i].addEventListener("click",
+  function() {
+    console.log("click ok");
+  })
+};
